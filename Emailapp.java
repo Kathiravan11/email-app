887456807b2295cc8eb0d9a049b17118bd7d4462
Kathiravan11 <133109509+Kathiravan11@users.noreply.github.com>
2023-08-07 16:38:20 +0530

    Add files via upload

---
+++ Emailapp.java
@@ -0,0 +1,15 @@
+package emailapp;
+
+public class Emailapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Email em1 = new Email("John", "Smith");
+		System.out.println(em1.showInfo());
+		
+//		em1.setAlternateEmail("js@gmail.com");
+//		System.out.println(em1.getAlternateMail());
+	}
+
+}
